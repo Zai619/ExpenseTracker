664fb36bb1cb3d9002d6936503057ef6ac568f24
Zai619 <tracycarriemomo@gmail.com>
2026-06-20 21:30:35 +0800

    加入專案檔案。

---
+++ ExpenseTracker.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ExpenseTracker
+{
+    partial class ExpenseTracker
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgvAccount = new System.Windows.Forms.DataGridView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMemo = new System.Windows.Forms.TextBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.cmbCategory = new System.Windows.Forms.ComboBox();
+            this.dtpDate = new System.Windows.Forms.DateTimePicker();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rdoDelete = new System.Windows.Forms.RadioButton();
+            this.rdoUpdate = new System.Windows.Forms.RadioButton();
+            this.rdoAdd = new System.Windows.Forms.RadioButton();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            this.cmbFilterCategory = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccount)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dgvAccount
+            // 
+            this.dgvAccount.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvAccount.Location = new System.Drawing.Point(33, 102);
+            this.dgvAccount.Name = "dgvAccount";
+            this.dgvAccount.RowHeadersWidth = 51;
+            this.dgvAccount.RowTemplate.Height = 27;
+            this.dgvAccount.Size = new System.Drawing.Size(519, 150);
+            this.dgvAccount.TabIndex = 0;
+            this.dgvAccount.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvAccount_CellClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtMemo);
+            this.groupBox1.Controls.Add(this.txtAmount);
+            this.groupBox1.Controls.Add(this.cmbCategory);
+            this.groupBox1.Controls.Add(this.dtpDate);
+            this.groupBox1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.groupBox1.Location = new System.Drawing.Point(569, 40);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(286, 367);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "帳目資料";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(23, 246);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "備註";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(23, 178);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "金額";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(52, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "類別";
+            // 
+            // txtMemo
+            // 
+            this.txtMemo.Location = new System.Drawing.Point(103, 237);
+            this.txtMemo.Name = "txtMemo";
+            this.txtMemo.Size = new System.Drawing.Size(100, 34);
+            this.txtMemo.TabIndex = 3;
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(103, 178);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(100, 34);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // cmbCategory
+            // 
+            this.cmbCategory.FormattingEnabled = true;
+            this.cmbCategory.Items.AddRange(new object[] {
+            "食",
+            "衣",
+            "住",
+            "行",
+            "育",
+            "樂",
+            "其他"});
+            this.cmbCategory.Location = new System.Drawing.Point(149, 97);
+            this.cmbCategory.Name = "cmbCategory";
+            this.cmbCategory.Size = new System.Drawing.Size(121, 33);
+            this.cmbCategory.TabIndex = 1;
+            // 
+            // dtpDate
+            // 
+            this.dtpDate.Location = new System.Drawing.Point(3, 30);
+            this.dtpDate.Name = "dtpDate";
+            this.dtpDate.Size = new System.Drawing.Size(200, 34);
+            this.dtpDate.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdoDelete);
+            this.groupBox2.Controls.Add(this.rdoUpdate);
+            this.groupBox2.Controls.Add(this.rdoAdd);
+            this.groupBox2.Location = new System.Drawing.Point(113, 325);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "操作功能";
+            // 
+            // rdoDelete
+            // 
+            this.rdoDelete.AutoSize = true;
+            this.rdoDelete.Location = new System.Drawing.Point(6, 71);
+            this.rdoDelete.Name = "rdoDelete";
+            this.rdoDelete.Size = new System.Drawing.Size(88, 19);
+            this.rdoDelete.TabIndex = 2;
+            this.rdoDelete.Text = "刪除資料";
+            this.rdoDelete.UseVisualStyleBackColor = true;
+            this.rdoDelete.Click += new System.EventHandler(this.rdoDelete_Click);
+            // 
+            // rdoUpdate
+            // 
+            this.rdoUpdate.AutoSize = true;
+            this.rdoUpdate.Location = new System.Drawing.Point(6, 46);
+            this.rdoUpdate.Name = "rdoUpdate";
+            this.rdoUpdate.Size = new System.Drawing.Size(88, 19);
+            this.rdoUpdate.TabIndex = 1;
+            this.rdoUpdate.Text = "修改資料";
+            this.rdoUpdate.UseVisualStyleBackColor = true;
+            this.rdoUpdate.Click += new System.EventHandler(this.rdoUpdate_Click);
+            // 
+            // rdoAdd
+            // 
+            this.rdoAdd.AutoSize = true;
+            this.rdoAdd.Checked = true;
+            this.rdoAdd.Location = new System.Drawing.Point(3, 21);
+            this.rdoAdd.Name = "rdoAdd";
+            this.rdoAdd.Size = new System.Drawing.Size(88, 19);
+            this.rdoAdd.TabIndex = 0;
+            this.rdoAdd.TabStop = true;
+            this.rdoAdd.Text = "新增資料";
+            this.rdoAdd.UseVisualStyleBackColor = true;
+            this.rdoAdd.Click += new System.EventHandler(this.rdoAdd_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(355, 325);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 3;
+            this.btnShow.Text = "顯示帳目";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(355, 384);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 4;
+            this.btnRun.Text = "執行";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Location = new System.Drawing.Point(57, 62);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(200, 25);
+            this.dtpStart.TabIndex = 5;
+            this.dtpStart.ValueChanged += new System.EventHandler(this.dtpStart_ValueChanged);
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Location = new System.Drawing.Point(284, 72);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(200, 25);
+            this.dtpEnd.TabIndex = 6;
+            this.dtpEnd.ValueChanged += new System.EventHandler(this.dtpEnd_ValueChanged);
+            // 
+            // cmbFilterCategory
+            // 
+            this.cmbFilterCategory.FormattingEnabled = true;
+            this.cmbFilterCategory.Items.AddRange(new object[] {
+            "全部",
+            "食",
+            "衣",
+            "住",
+            "行",
+            "育",
+            "樂",
+            "其他"});
+            this.cmbFilterCategory.Location = new System.Drawing.Point(456, 23);
+            this.cmbFilterCategory.Name = "cmbFilterCategory";
+            this.cmbFilterCategory.Size = new System.Drawing.Size(96, 23);
+            this.cmbFilterCategory.TabIndex = 7;
+            this.cmbFilterCategory.SelectedIndexChanged += new System.EventHandler(this.cmbFilterCategory_SelectedIndexChanged);
+            // 
+            // ExpenseTracker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(904, 450);
+            this.Controls.Add(this.cmbFilterCategory);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dgvAccount);
+            this.Name = "ExpenseTracker";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "個人記帳系統";
+            this.Load += new System.EventHandler(this.ExpenseTracker_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAccount)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvAccount;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox cmbCategory;
+        private System.Windows.Forms.DateTimePicker dtpDate;
+        private System.Windows.Forms.TextBox txtMemo;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdoAdd;
+        private System.Windows.Forms.RadioButton rdoDelete;
+        private System.Windows.Forms.RadioButton rdoUpdate;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+        private System.Windows.Forms.ComboBox cmbFilterCategory;
+    }
+}
+
---
+++ ExpenseTracker.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ExpenseTracker
+{
+    public partial class ExpenseTracker : Form
+    {
+        SqlConnection sqlDb = null;
+        string cntStr = @"Data Source=(localDB)\MSSQLLocalDB;" +
+                        @"AttachDBFilename=|DataDirectory|\account.mdf;";
+        int func = 1;
+
+        public ExpenseTracker()
+        {
+            InitializeComponent();
+        }
+
+        private void ExpenseTracker_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                sqlDb = new SqlConnection(cntStr);
+                sqlDb.Open();
+                dgvAccount.RowHeadersVisible = false;
+                dgvAccount.AllowUserToAddRows = false;
+
+                // 核心防呆：當資料庫撈出的分類不在選單清單中時，默默吸收錯誤不閃退
+                dgvAccount.DataError += (s, ev) => { ev.ThrowException = false; };
+
+                dtpStart.Value = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+                dtpEnd.Value = DateTime.Now;
+                // 連線成功，預先載入一次資料
+                if (cmbFilterCategory.Items.Count > 0)
+                {
+                    cmbFilterCategory.SelectedIndex = 0;
+                }
+
+                // 3. 程式啟動時，直接執行連動篩選！
+                AutoFilter();
+                ShowData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("提醒：本專案使用 LocalDB 服務，若未安裝可能無法正常讀寫資料庫。\n錯誤訊息：" + ex.Message,
+                                "系統連線提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                btnRun.Enabled = false;
+                btnShow.Enabled = false;
+            }
+        }
+
+        private void ShowData()
+        {
+            dgvAccount.Rows.Clear();
+            dgvAccount.Columns.Clear();
+
+            dgvAccount.Columns.Add("Id", "序號");
+            dgvAccount.Columns.Add("日期", "日期");
+
+            // 建立下拉選單欄位
+            DataGridViewComboBoxColumn cmbCol = new DataGridViewComboBoxColumn();
+            cmbCol.Name = "分類";
+            cmbCol.HeaderText = "分類";
+            cmbCol.Items.Add("食");
+            cmbCol.Items.Add("衣");
+            cmbCol.Items.Add("住");
+            cmbCol.Items.Add("行");
+            cmbCol.Items.Add("育");
+            cmbCol.Items.Add("樂");
+            cmbCol.Items.Add("其他");
+            cmbCol.Items.Add("午餐"); // 補上測試資料的分類，避免顯示空白
+            dgvAccount.Columns.Add(cmbCol);
+
+            dgvAccount.Columns.Add("金額", "金額");
+            dgvAccount.Columns.Add("備註", "備註");
+
+            dgvAccount.Columns["Id"].Width = 60;
+            dgvAccount.Columns["金額"].Width = 80;
+
+            try
+            {
+                string sqlStr = "SELECT * FROM accountBook ORDER BY 日期 DESC";
+                SqlCommand sqlCmd = new SqlCommand(sqlStr, sqlDb);
+                SqlDataReader sqlDr = sqlCmd.ExecuteReader();
+
+                while (sqlDr.Read())
+                {
+                    dgvAccount.Rows.Add(
+                        sqlDr["Id"].ToString(),
+                        Convert.ToDateTime(sqlDr["日期"]).ToShortDateString(),
+                        sqlDr["分類"].ToString().Trim(),
+                        sqlDr["金額"].ToString(),
+                        sqlDr["備註"].ToString()
+                    );
+                }
+                sqlDr.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("讀取資料庫失敗：" + ex.Message);
+            }
+        }
+        private void ShowData(DateTime startDate, DateTime endDate, string category)
+        {
+            // 1. 清空表格
+            dgvAccount.Rows.Clear();
+            dgvAccount.Columns.Clear();
+
+            // 2. 建立標題欄位
+            dgvAccount.Columns.Add("Id", "序號");
+            dgvAccount.Columns.Add("日期", "日期");
+            dgvAccount.Columns.Add("分類", "分類");
+            dgvAccount.Columns.Add("金額", "金額");
+            dgvAccount.Columns.Add("備註", "備註");
+            dgvAccount.Columns["Id"].Width = 60;
+            dgvAccount.Columns["金額"].Width = 80;
+
+            try
+            {
+                // 3. 利用 WHERE 組合多條件篩選 SQL 指令 (對應投影片 WHERE 查詢條件概念)
+                // 基本條件：篩選在 startDate 與 endDate 之間的日期
+                string sqlStr = "SELECT * FROM accountBook WHERE 日期 BETWEEN @start AND @end";
+
+                // 如果類別不是選擇「全部」，就再用 AND 串接類別條件
+                if (category != "全部" && !string.IsNullOrEmpty(category))
+                {
+                    sqlStr += " AND 分類 = @category";
+                }
+
+                // 最後加上最新日期排序
+                sqlStr += " ORDER BY 日期 DESC";
+
+                // 4. 使用參數化查詢（防範 SQL 注入，也避免日期字串格式出錯）
+                SqlCommand sqlCmd = new SqlCommand(sqlStr, sqlDb);
+                sqlCmd.Parameters.AddWithValue("@start", startDate.ToString("yyyy-MM-dd"));
+                sqlCmd.Parameters.AddWithValue("@end", endDate.ToString("yyyy-MM-dd"));
+
+                if (category != "全部" && !string.IsNullOrEmpty(category))
+                {
+                    sqlCmd.Parameters.AddWithValue("@category", category);
+                }
+
+                // 5. 執行並透過 Reader 讀取資料填入表格 (對應投影片 Reader 邏輯)
+                SqlDataReader sqlDr = sqlCmd.ExecuteReader();
+                while (sqlDr.Read() != false)
+                {
+                    dgvAccount.Rows.Add(
+                        sqlDr["Id"].ToString(),
+                        Convert.ToDateTime(sqlDr["日期"]).ToShortDateString(),
+                        sqlDr["分類"].ToString().Trim(),
+                        sqlDr["金額"].ToString(),
+                        sqlDr["備註"].ToString()
+                    );
+                }
+                sqlDr.Close(); // 務必關閉 Reader
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("篩選失敗：" + ex.Message);
+            }
+        }
+        private void AutoFilter()
+        {
+            DateTime start = dtpStart.Value;
+            DateTime end = dtpEnd.Value;
+            string cat = cmbFilterCategory.Text;
+
+            // 如果開始日期大於結束日期，就不動作（防呆）
+            if (start > end) return;
+
+            // 自動執行我們在上一步寫好的篩選 ShowData
+            ShowData(start, end, cat);
+        }
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            ShowData();
+        }
+
+        private void rdoAdd_Click(object sender, EventArgs e)
+        {
+            func = 1;
+            dtpDate.Enabled = true;
+            cmbCategory.Enabled = true;
+            txtAmount.Enabled = true;
+            txtMemo.Enabled = true;
+        }
+
+        private void rdoUpdate_Click(object sender, EventArgs e)
+        {
+            func = 2;
+            dtpDate.Enabled = true;
+            cmbCategory.Enabled = true;
+            txtAmount.Enabled = true;
+            txtMemo.Enabled = true;
+        }
+        private void rdoDelete_Click(object sender, EventArgs e)
+        {
+            func = 3;
+            dtpDate.Enabled = false;
+            cmbCategory.Enabled = false;
+            txtAmount.Enabled = false;
+            txtMemo.Enabled = false;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            if ((func == 1 || func == 2) && string.IsNullOrEmpty(txtAmount.Text))
+            {
+                MessageBox.Show("請輸入金額！", "提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+
+            string sqlStr = "";
+
+            switch (func)
+            {
+                case 1:
+                    sqlStr = $"INSERT INTO accountBook (日期, 分類, 金額, 備註) VALUES ('{dtpDate.Value:yyyy-MM-dd}', N'{cmbCategory.Text}', {txtAmount.Text}, N'{txtMemo.Text}')";
+                    break;
+
+                case 2:
+                    if (dgvAccount.CurrentRow == null || dgvAccount.CurrentRow.IsNewRow || dgvAccount.CurrentRow.Cells["Id"].Value == null) return;
+                    string updateId = dgvAccount.CurrentRow.Cells["Id"].Value.ToString();
+                    sqlStr = $"UPDATE accountBook SET 日期='{dtpDate.Value:yyyy-MM-dd}', 分類=N'{cmbCategory.Text}', 金額={txtAmount.Text}, 備註=N'{txtMemo.Text}' WHERE Id = {updateId}";
+                    break;
+
+                case 3:
+                    if (dgvAccount.CurrentRow == null || dgvAccount.CurrentRow.IsNewRow || dgvAccount.CurrentRow.Cells["Id"].Value == null) return;
+                    string deleteId = dgvAccount.CurrentRow.Cells["Id"].Value.ToString();
+                    sqlStr = $"DELETE FROM accountBook WHERE Id = {deleteId}";
+                    break;
+            }
+
+            try
+            {
+                SqlCommand cmd = new SqlCommand(sqlStr, sqlDb);
+                cmd.ExecuteNonQuery();
+
+                string msg = func == 1 ? "資料已新增" : (func == 2 ? "資料已更新" : "資料已刪除");
+                MessageBox.Show(msg, "系統提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                txtAmount.Clear();
+                txtMemo.Clear();
+                ShowData();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("執行失敗：" + ex.Message, "錯誤", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void dgvAccount_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0 && !dgvAccount.Rows[e.RowIndex].IsNewRow)
+            {
+                DataGridViewRow row = dgvAccount.Rows[e.RowIndex];
+
+                if (row.Cells["日期"].Value != null)
+                    dtpDate.Value = Convert.ToDateTime(row.Cells["日期"].Value);
+
+                cmbCategory.Text = row.Cells["分類"].Value?.ToString().Trim() ?? "";
+                txtAmount.Text = row.Cells["金額"].Value?.ToString() ?? "";
+                txtMemo.Text = row.Cells["備註"].Value?.ToString() ?? "";
+            }
+        }
+
+        // 表格內選單變更時自動同步寫入資料庫
+        private void dgvAccount_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // 確保改到的是「分類」欄位 (index = 2) 且連線存在
+            if (e.RowIndex >= 0 && e.ColumnIndex == 2 && sqlDb != null && sqlDb.State == ConnectionState.Open)
+            {
+                DataGridViewRow row = dgvAccount.Rows[e.RowIndex];
+                if (row.Cells["Id"].Value != null && row.Cells["分類"].Value != null)
+                {
+                    string recordId = row.Cells["Id"].Value.ToString();
+                    string newCategory = row.Cells["分類"].Value.ToString();
+
+                    try
+                    {
+                        string updateSql = $"UPDATE accountBook SET 分類 = N'{newCategory}' WHERE Id = {recordId}";
+                        SqlCommand cmd = new SqlCommand(updateSql, sqlDb);
+                        cmd.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("同步資料庫失敗：" + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void dtpStart_ValueChanged(object sender, EventArgs e)
+        {
+            AutoFilter();
+        }
+
+        private void dtpEnd_ValueChanged(object sender, EventArgs e)
+        {
+            AutoFilter();
+        }
+
+        private void cmbFilterCategory_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            AutoFilter();
+
+        }
+    }
+}
